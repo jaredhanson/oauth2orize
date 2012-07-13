cd1c812a2869c12ce9794b22f55a817b1f2098ee
Jared Hanson <jaredhanson@gmail.com>
2012-07-12 20:05:24 -0700

    Test cases for deserializeClient.

--- test/server-test.js
+++ test/server-test.js
@@ -153,4 +153,189 @@ vows.describe('Server').addBatch({
     },
   },
   
+  'server with no deserializers': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should fail to deserialize client': function (err, client) {
+      assert.instanceOf(err, Error);
+      assert.equal(err.message, 'Failed to deserialize client.  Register deserialization function using deserializeClient().');
+      assert.isUndefined(client);
+    },
+  },
+  
+  'server with one deserializer': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(id, done) {
+        done(null, { id: id });
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should deserialize client': function (err, client) {
+      assert.isNull(err);
+      assert.equal(client.id, '1');
+    },
+  },
+  
+  'server with multiple deserializers': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(id, done) {
+        done('pass');
+      });
+      server.deserializeClient(function(id, done) {
+        done(null, 'second-deserializer');
+      });
+      server.deserializeClient(function(id, done) {
+        done(null, 'should-not-execute');
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should deserialize client': function (err, client) {
+      assert.isNull(err);
+      assert.equal(client, 'second-deserializer');
+    },
+  },
+  
+  'server with one deserializer that sets client to null': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(id, done) {
+        done(null, null);
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should invalidate client': function (err, client) {
+      assert.isNull(err);
+      assert.strictEqual(client, false);
+    },
+  },
+  
+  'server with one deserializer that sets client to false': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(id, done) {
+        done(null, false);
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should invalidate client': function (err, client) {
+      assert.isNull(err);
+      assert.strictEqual(client, false);
+    },
+  },
+  
+  'server with multiple deserializers, the second of which sets client to null': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(obj, done) {
+        done('pass');
+      });
+      server.deserializeClient(function(obj, done) {
+        done(null, null);
+      });
+      server.deserializeClient(function(obj, done) {
+        done(null, 'should-not-execute');
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should invalidate client': function (err, client) {
+      assert.isNull(err);
+      assert.strictEqual(client, false);
+    },
+  },
+  
+  'server with multiple deserializers, the second of which sets client to false': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(obj, done) {
+        done('pass');
+      });
+      server.deserializeClient(function(obj, done) {
+        done(null, false);
+      });
+      server.deserializeClient(function(obj, done) {
+        done(null, 'should-not-execute');
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should invalidate client': function (err, client) {
+      assert.isNull(err);
+      assert.strictEqual(client, false);
+    },
+  },
+  
+  'server with a deserializer that throws an error': {
+    topic: function() {
+      var self = this;
+      var server = new Server();
+      server.deserializeClient(function(obj, done) {
+        // throws ReferenceError: wtf is not defined
+        wtf
+      });
+      function deserialized(err, client) {
+        self.callback(err, client);
+      }
+      process.nextTick(function () {
+        server.deserializeClient('1', deserialized);
+      });
+    },
+    
+    'should fail to deserialize client': function (err, obj) {
+      assert.instanceOf(err, Error);
+      assert.isUndefined(obj);
+    },
+  },
+  
 }).export(module);
