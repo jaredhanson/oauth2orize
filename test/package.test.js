2289529fd130749f7208ad953c52573030ba2e90
Jared Hanson <jaredhanson@gmail.com>
2013-08-27 08:22:11 -0700

    Export AuthorizationError.

--- test/package.test.js
+++ test/package.test.js
@@ -46,12 +46,8 @@ describe('oauth2orize', function() {
     
   });
   
-  /*
   it('should export Error constructors', function() {
-    expect(strategy.AuthorizationError).to.be.a('function');
-    expect(strategy.TokenError).to.be.a('function');
-    expect(strategy.InternalOAuthError).to.be.a('function');
+    expect(oauth2orize.AuthorizationError).to.be.a('function');
   });
-  */
   
 });
