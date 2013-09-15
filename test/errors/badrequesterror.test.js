7c11d8ffcbd04b68500a1239bfa45c30cfc8ef81
Jared Hanson <jaredhanson@gmail.com>
2013-09-15 10:09:24 -0700

    Add test case for BadRequestError.

---
+++ test/errors/badrequesterror.test.js
@@ -0,0 +1,31 @@
+var BadRequestError = require('../../lib/errors/badrequesterror');
+
+
+describe('BadRequestError', function() {
+    
+  describe('constructed without a message', function() {
+    var err = new BadRequestError();
+    
+    it('should have default properties', function() {
+      expect(err.message).to.be.undefined;
+    });
+    
+    it('should format correctly', function() {
+      //expect(err.toString()).to.equal('BadRequestError');
+      expect(err.toString().indexOf('BadRequestError')).to.equal(0);
+    });
+  });
+  
+  describe('constructed with a message', function() {
+    var err = new BadRequestError('Bad request');
+    
+    it('should have default properties', function() {
+      expect(err.message).to.equal('Bad request');
+    });
+    
+    it('should format correctly', function() {
+      expect(err.toString()).to.equal('BadRequestError: Bad request');
+    });
+  });
+  
+});
