3ffaf14ed3178dd03ba061defe77256fd28b1942
Jared Hanson <jaredhanson@gmail.com>
2013-09-18 15:26:08 -0700

    Add test cases for ForbiddenError.

--- test/errors/forbiddenerror.test.js
+++ test/errors/forbiddenerror.test.js
@@ -1,38 +1,38 @@
-var BadRequestError = require('../../lib/errors/badrequesterror');
+var ForbiddenError = require('../../lib/errors/forbiddenerror');
 
 
-describe('BadRequestError', function() {
+describe('ForbiddenError', function() {
     
   describe('constructed without a message', function() {
-    var err = new BadRequestError();
+    var err = new ForbiddenError();
     
     it('should have default properties', function() {
       expect(err.message).to.be.undefined;
     });
     
     it('should format correctly', function() {
-      //expect(err.toString()).to.equal('BadRequestError');
-      expect(err.toString().indexOf('BadRequestError')).to.equal(0);
+      //expect(err.toString()).to.equal('ForbiddenError');
+      expect(err.toString().indexOf('ForbiddenError')).to.equal(0);
     });
     
     it('should have status', function() {
-      expect(err.status).to.equal(400);
+      expect(err.status).to.equal(403);
     });
   });
   
   describe('constructed with a message', function() {
-    var err = new BadRequestError('Bad request');
+    var err = new ForbiddenError('Forbidden');
     
     it('should have default properties', function() {
-      expect(err.message).to.equal('Bad request');
+      expect(err.message).to.equal('Forbidden');
     });
     
     it('should format correctly', function() {
-      expect(err.toString()).to.equal('BadRequestError: Bad request');
+      expect(err.toString()).to.equal('ForbiddenError: Forbidden');
     });
     
     it('should have status', function() {
-      expect(err.status).to.equal(400);
+      expect(err.status).to.equal(403);
     });
   });
   
