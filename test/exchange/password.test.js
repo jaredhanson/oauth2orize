b92f6b26e4ce15a8a59f5e860fc1d77471846a32
Jared Hanson <jaredhanson@gmail.com>
2016-07-07 11:37:35 -0300

    Clarify password test cases.

--- test/exchange/password.test.js
+++ test/exchange/password.test.js
@@ -226,6 +226,44 @@ describe('exchange.password', function() {
     });
   });
   
+  describe('issuing an access token based on array of scopes', function() {
+    function issue(client, username, passwd, scope, done) {
+      if (client.id !== 'c123') { return done(new Error('incorrect client argument')); }
+      if (username !== 'bob') { return done(new Error('incorrect username argument')); }
+      if (passwd !== 'shh') { return done(new Error('incorrect passwd argument')); }
+      if (scope.length !== 2) { return done(new Error('incorrect scope argument')); }
+      if (scope[0] !== 'read') { return done(new Error('incorrect scope argument')); }
+      if (scope[1] !== 'write') { return done(new Error('incorrect scope argument')); }
+      
+      return done(null, 's3cr1t')
+    }
+    
+    var response, err;
+
+    before(function(done) {
+      chai.connect.use(password(issue))
+        .req(function(req) {
+          req.user = { id: 'c123', name: 'Example' };
+          req.body = { username: 'bob', password: 'shh', scope: 'read write' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
+    });
+  });
+  
   describe('issuing an access token based on scope and body', function() {
     function issue(client, username, passwd, scope, body, done) {
       if (client.id !== 'c123') { return done(new Error('incorrect client argument')); }
@@ -299,44 +337,6 @@ describe('exchange.password', function() {
       expect(response.getHeader('Pragma')).to.equal('no-cache');
     });
 
-    it('should respond with body', function() {
-      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
-    });
-  });
-  
-  describe('issuing an access token based on array of scopes', function() {
-    function issue(client, username, passwd, scope, done) {
-      if (client.id !== 'c123') { return done(new Error('incorrect client argument')); }
-      if (username !== 'bob') { return done(new Error('incorrect username argument')); }
-      if (passwd !== 'shh') { return done(new Error('incorrect passwd argument')); }
-      if (scope.length !== 2) { return done(new Error('incorrect scope argument')); }
-      if (scope[0] !== 'read') { return done(new Error('incorrect scope argument')); }
-      if (scope[1] !== 'write') { return done(new Error('incorrect scope argument')); }
-      
-      return done(null, 's3cr1t')
-    }
-    
-    var response, err;
-
-    before(function(done) {
-      chai.connect.use(password(issue))
-        .req(function(req) {
-          req.user = { id: 'c123', name: 'Example' };
-          req.body = { username: 'bob', password: 'shh', scope: 'read write' };
-        })
-        .end(function(res) {
-          response = res;
-          done();
-        })
-        .dispatch();
-    });
-    
-    it('should respond with headers', function() {
-      expect(response.getHeader('Content-Type')).to.equal('application/json');
-      expect(response.getHeader('Cache-Control')).to.equal('no-store');
-      expect(response.getHeader('Pragma')).to.equal('no-cache');
-    });
-    
     it('should respond with body', function() {
       expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
     });
