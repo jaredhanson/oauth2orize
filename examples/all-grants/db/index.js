40af8d7a9707c115ce41188d344f6552151873d5
Gerges Beshay <gerges.beshay@bluescape.com>
2016-10-19 10:13:21 -0700

    Move examples to another repo

--- examples/all-grants/db/index.js
+++
@@ -1,5 +0,0 @@
-exports.users = require('./users');
-exports.clients = require('./clients');
-exports.accessTokens = require('./accesstokens');
-exports.authorizationCodes = require('./authorizationcodes');
-
